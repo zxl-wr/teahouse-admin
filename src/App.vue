4151279b73f70da77d1e05233180f38981b953b9
zhxiaolu <58330037+zhxiaolu@users.noreply.github.com>
2023-05-21 10:11:01 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-container class="h-100vh">
+    <el-aside class="bg-#f6f6f7" width="200px"> </el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import UnoCSS from "unocss/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    UnoCSS(),
+    AutoImport({
+      imports: [
+        // 插件预设支持导入的api
+        "vue",
+        "vue-router",
+        "pinia",
+        // 自定义导入的api
+      ],
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+});
