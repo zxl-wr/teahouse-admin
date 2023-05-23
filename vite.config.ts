255d8b791570e4ea6833b0778b4ec880e5a79b6a
zxl-wr <1248485827@qq.com>
2023-05-23 10:30:04 +0800

    优化文件结构

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,49 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-import UnoCSS from "unocss/vite";
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import UnoCSS from 'unocss/vite';
+import { presetUno } from 'unocss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    UnoCSS(),
+    UnoCSS({
+      // UnoCSS 预设
+      presets: [presetUno()],
+      // 自定义规则
+      rules: [
+        ['flex-row', { display: 'flex', 'flex-direction': 'row' }],
+        ['flex-row-center', { display: 'flex', 'flex-direction': 'row', 'align-items': 'center' }],
+        ['flex-row-between', { display: 'flex', 'flex-direction': 'row', 'align-items': 'center', 'justify-content': 'space-between' }],
+        ['flex-column', { display: 'flex', 'flex-direction': 'column', 'align-items': 'center' }],
+        ['flex-column-center', { display: 'flex', 'flex-direction': 'column', 'align-items': 'center', 'justify-content': 'center' }],
+        ['flex-column-between', { display: 'flex', 'flex-direction': 'column', 'align-items': 'center', 'justify-content': 'space-between' }],
+      ],
+    }),
     AutoImport({
       imports: [
         // 插件预设支持导入的api
-        "vue",
-        "vue-router",
-        "pinia",
+        'vue',
+        'vue-router',
+        'pinia',
         // 自定义导入的api
       ],
       resolvers: [ElementPlusResolver()],
+      dts: 'config/auto-imports.d.ts',
     }),
     Components({
       resolvers: [ElementPlusResolver()],
+      dts: 'config/components.d.ts',
     }),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
