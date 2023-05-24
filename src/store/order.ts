9330a6ee541370f21d673c237d17bdc569d7be32
zxl-wr <1248485827@qq.com>
2023-05-24 23:51:59 +0800

    优化log

--- src/store/order.ts
+++ src/store/order.ts
@@ -1,17 +1,17 @@
-import { defineStore } from 'pinia';
-import type { Order } from '@/assets/type';
+import { defineStore } from "pinia";
+import type { Order } from "@/assets/type";
 
 // 优先查看缓存里的订单
-const localOrder: string = localStorage.getItem('order') || '[]';
+const localOrder: string = localStorage.getItem("order") || "[]";
 const _arr: Order[] = JSON.parse(localOrder);
 
 // 存入缓存
 function setLocalStorage(value: Order[]) {
   const _str: string = JSON.stringify(value); // 转化为字符串
-  localStorage.setItem('order', _str); // 存入缓存
+  localStorage.setItem("order", _str); // 存入缓存
 }
 
-export const useOrderStore = defineStore('order', () => {
+export const useOrderStore = defineStore("order", () => {
   const currentOrderList = ref<Order[]>(_arr); // 当前订单
 
   // 新增订单
@@ -22,9 +22,9 @@ export const useOrderStore = defineStore('order', () => {
 
   // 结束订单
   function endOrder(tableId: string | number) {
-    currentOrderList.value = currentOrderList.value.filter((item) => item.tableId != tableId); // 过滤出该桌台的订单
-    console.log(currentOrderList.value);
-    
+    currentOrderList.value = currentOrderList.value.filter(
+      (item) => item.tableId != tableId
+    ); // 过滤出该桌台的订单
     setLocalStorage(currentOrderList.value);
   }
 
