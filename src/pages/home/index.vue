8be522cf429b9461353a9496f88b1ae80edfbc77
zxl-wr <1248485827@qq.com>
2023-05-29 23:53:56 +0800

    消费、结算功能初步完善

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -9,19 +9,25 @@
   <!-- 消详情抽屉 -->
   <el-drawer v-model="isShowDawer" direction="btt" size="300px" :with-header="false">
     <div class="flex-row-between">
-      <el-table style="width: 620px" :data="currentOrder?.goods_list" height="260" empty-text="暂无消费">
-        <el-table-column prop="goods_id" label="序号" />
+      <el-table style="width: 620px" :data="currentOrder?.goods_list" height="260" table-layout="auto" empty-text="暂无消费">
         <el-table-column prop="name" label="名称" />
         <el-table-column prop="price" label="价格" />
         <el-table-column prop="number" label="数量" />
+        <el-table-column prop="time_at" label="消费时间">
+          <template #default="scope">
+            {{ useDateFormat(scope.row.time_at, 'MM-DD HH:mm').value }}
+          </template>
+        </el-table-column>
         <el-table-column label="操作">
-          <el-button type="danger">删除</el-button>
+          <template #default="scope">
+            <el-button type="danger" @click="deleteOrderGoods(scope.row)">删除</el-button>
+          </template>
         </el-table-column>
       </el-table>
       <div class="w-500px h-260px" v-if="currentOrder">
         <div class="text-6">开始时间：{{ useDateFormat(currentOrder.start_at, 'YYYY-MM-DD HH:mm:ss').value }}</div>
         <div class="mt-8 text-12 text-center">{{ differ }}</div>
-        <div class="mb-8 text-8 text-center c-#f4516c">￥200</div>
+        <div class="mb-8 text-8 text-center c-#f4516c">￥{{ currentOrderPrice }}</div>
         <el-button class="float-right !w-120px !h-48px !text-5" type="danger" size="large" @click="isShowDialog = true">
           结算
         </el-button>
@@ -33,8 +39,8 @@
     </div>
   </el-drawer>
   <!-- 新增消费 -->
-  <el-dialog v-model="isShowTransfer" title="新增消费" width="900" align-center :close-on-click-modal="false"
-    :close-on-press-escape="false">
+  <el-dialog v-model="isShowTransfer" title="新增消费" width="900" align-center :show-close="false"
+    :close-on-click-modal="false" :close-on-press-escape="false">
     <el-select class="mb-4" v-model="currentGoodsTypeId" placeholder="请选择类型">
       <el-option v-for="item in goodsType.filter((i) => i.id >= 0)" :key="item.id" :label="item.name" :value="item.id" />
     </el-select>
@@ -56,6 +62,10 @@
         </el-table-column>
       </el-table>
     </div>
+    <template #footer>
+      <el-button @click="isShowTransfer = false">取消</el-button>
+      <el-button type="primary" @click="updateOrder">确定</el-button>
+    </template>
   </el-dialog>
   <!-- 结算弹窗 -->
   <el-dialog v-model="isShowDialog" title="结账">
@@ -69,7 +79,7 @@
         </template>
       </el-table-column>
     </el-table>
-    <div class="mx-8 text-8 text-end c-#f4516c">￥200</div>
+    <div class="mx-8 text-8 text-end c-#f4516c">￥{{ currentOrderPrice }}</div>
     <template #footer>
       <el-button @click="isShowDialog = false">取消</el-button>
       <el-button type="danger" @click="clickEndOrder">确定</el-button>
@@ -115,7 +125,7 @@ const clickTable = async (tableId: number) => {
     const _timestamp = new Date().getTime(); // 开始时间
     const _orderId = _timestamp + '-' + tableId; // 订单id    
     await db.order_store.add({ id: _orderId, table_id: tableId, start_at: _timestamp, end_at: -1 });
-    getCurrentOrderList();
+    await getCurrentOrderList();
   }
   currentOrder.value = currentOrderList.value.find((item) => item.table_id == tableId); // 当前订单
   isShowDawer.value = true; // 打开消费详情抽屉
@@ -143,8 +153,8 @@ watch(timestamp, (newValue) => {
 /**
  * 消费功能
  */
-import { goodsType } from '@/assets/constant.ts';
 import type { Goods, Order_Goods } from "@/assets/type.ts";
+import { goodsType } from '@/assets/constant.ts';
 const isShowTransfer = ref(false); // 是否打开新增消费穿梭框
 const currentGoodsTypeId = ref<number>(); // 当前商品类型id
 const currentGoodsList = ref<Goods[]>(); // 当前类型中的商品列表
@@ -174,17 +184,76 @@ const addGoods = () => {
       name: item.name, price: item.price, number: 1, time_at: timestamp.value,
     })
   }
+  currentGoodsTypeId.value = undefined
+};
+// 更新订单
+const updateOrder = async () => {
+  const _currentOrder = await db.order_store.get({ id: currentOrder.value?.id });
+  if (!_currentOrder) return;
+  if (!_currentOrder.goods_list) _currentOrder.goods_list = [];
+  newGoods.value.forEach(item => _currentOrder.goods_list?.push({ ...item }))
+  db.order_store.update(_currentOrder.id, _currentOrder)
+    .then(() => {
+      currentOrder.value = _currentOrder;
+      getCurrentOrderList()
+    })
+    .catch(error => console.log(error)
+    );
+  isShowTransfer.value = false
+}
+// 删除消费
+const deleteOrderGoods = async (e: Order_Goods) => {
+  const _currentOrder = await db.order_store.get({ id: currentOrder.value?.id });
+  if (!_currentOrder?.goods_list) return;
+  const _i = getIndexInArr(_currentOrder.goods_list, { ...e });
+  _currentOrder.goods_list.splice(_i, 1);
+  db.order_store.update(_currentOrder.id, _currentOrder)
+    .then(() => {
+      currentOrder.value = _currentOrder;
+      getCurrentOrderList()
+    })
+    .catch(error => console.log(error)
+    );
+
+}
+//从数组中获取对象的索引
+const getIndexInArr = (_arr: object[], _obj: object) => {
+  for (var i = 0; i < _arr.length; i++) {
+    if (JSON.stringify(_arr[i]) == JSON.stringify(_obj)) {
+      return i;
+    }
+  }
+  return -1;
 };
 
 /**
  * 结算功能
  */
 const isShowDialog = ref(false);
-// const { endOrder } = orderStore;
 // 结算
-const clickEndOrder = () => {
-  isShowDawer.value = false;
-  // endOrder(currentOrder.value?.table_id);
-  isShowDialog.value = false;
+const clickEndOrder = async () => {
+  const _timestamp = new Date().getTime(); // 结束时间
+  const _currentOrder = await db.order_store.get({ id: currentOrder.value?.id });
+  if (!_currentOrder) return;
+  _currentOrder.end_at = _timestamp;
+  db.order_store.update(_currentOrder.id, _currentOrder)
+    .then(() => {
+      getCurrentOrderList()
+      isShowDialog.value = false;
+      isShowDawer.value = false;
+    })
+    .catch(error => console.log(error)
+    );
+
 };
+
+/**
+ * 统计收费
+ */
+const currentOrderPrice = computed(() => {
+  let _num = 0;
+  currentOrder.value?.goods_list?.forEach(item => _num += item.price)
+  return _num;
+})
+
 </script>
