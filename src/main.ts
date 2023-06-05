9fda77c304571b9042c3594e911317fa4b90650a
zxl-wr <1248485827@qq.com>
2023-06-05 17:55:58 +0800

    打印测试

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
+import print from 'vue3-print-nb';
 import 'virtual:uno.css';
 
 const app = createApp(App);
@@ -10,5 +11,7 @@ const pinia = createPinia();
 app.use(pinia);
 // 路由
 app.use(router);
+// 打印机
+app.use(print);
 
 app.mount('#app');
--- src/pages/print/index.vue
+++ src/pages/print/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>打印设置</div>
+  <div id="printMe">
+    <p>葫芦娃，葫芦娃</p>
+    <p>一根藤上七朵花</p>
+    <p>小小树藤是我家 啦啦啦啦</p>
+    <p>叮当当咚咚当当 浇不大</p>
+    <p>叮当当咚咚当当 是我家</p>
+    <p>啦啦啦啦</p>
+    <p>...</p>
+    <p>电话： {{ phone }} 地址：{{ address }}</p>
+  </div>
+
+  <el-button v-print="printObj"> 打印测试</el-button>
 </template>
+<script lang="ts" setup>
+const title = '天云茶楼';
+const phone = 18226163451;
+const address = '合肥市蜀山区江夏西路颐和花园';
+const printObj: any = { id: 'printMe', popTitle: title };
+</script>
