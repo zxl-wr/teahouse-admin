3409082986ec1a3a0f5527f0830b6bba1a4646bf
zxl <1248485827@qq.com>
2024-08-17 14:14:41 +0800

    update 升级版本依赖

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -29,24 +29,20 @@
   </div>
   <!-- 新增消费 -->
   <el-dialog style="width: fit-content" v-model="isShowTransfer" :title="getTableName(currentOrder.table_id)" top="0" v-if="currentOrder">
-    <div class="text-6 float-right c-#f56c6c">￥{{ getCurrentOrderPrice(currentOrder).toFixed(2) }}</div>
-    <!-- 商品类型选择 -->
-    <el-select class="mb-4" v-model="currentGoodsTypeId" placeholder="请选择类型">
-      <el-option v-for="item in goodsType" :key="item.id" :label="item.name" :value="item.id" />
-    </el-select>
-    <!-- 商品列表 -->
+    <div class="text-6 text-right c-#f56c6c">￥{{ getCurrentOrderPrice(currentOrder).toFixed(2) }}</div>
     <div class="flex">
+      <!-- 商品列表 -->
       <div class="w-300px h-400px overflow-auto">
-        <div
-          class="flex-row-between card m-1 p-2 select-none hover:bg-#2cbf9c hover:c-#fff"
-          v-for="goods in currentGoodsList"
-          :key="goods.id"
-          @dblclick="addOrderGoods(goods)">
+        <el-select class="mb-4" v-model="currentGoodsTypeId" placeholder="请选择类型">
+          <el-option v-for="item in goodsType" :key="item.id" :label="item.name" :value="item.id" />
+        </el-select>
+        <div class="flex-row-between card m-1 p-2 select-none hover:bg-#2cbf9c hover:c-#fff" v-for="goods in currentGoodsList" :key="goods.id" @dblclick="addOrderGoods(goods)">
           <div>{{ goods.name }}</div>
           <div>￥{{ goods.price }}</div>
         </div>
       </div>
-      <el-table class="!w-500px" :data="currentOrder.goods_list" border>
+      <!-- 已购商品 -->
+      <el-table class="!w-500px !h-400px" :data="currentOrder.goods_list" border>
         <el-table-column prop="name" label="已购商品" />
         <el-table-column prop="price" label="单价" width="60" />
         <el-table-column prop="number" label="数量" width="60" />
@@ -67,151 +63,165 @@
   <el-dialog style="width: fit-content" v-model="isShowBill" title="结账" top="0">
     <Bill id="printBill" :defaultOrder="currentOrder"></Bill>
     <template #footer>
-      <el-button type="danger" v-print="printObj" @click="printBill(currentOrder)">结账打印</el-button>
+      <el-button type="danger" v-print="printObj">结账打印</el-button>
     </template>
   </el-dialog>
 </template>
 
 <script lang="ts" setup>
 // #region 获取当前订单列表
-import type { Order } from '@/assets/type.ts'; // Order类型
-import { db } from '@/utils/indexDB.ts'; // 数据库
-import { defaultTables } from '@/assets/constant.ts'; // 默认桌台
-const currentOrderList = ref<Order[]>([]); // 当前订单列表
+import type { Order } from '@/assets/type.ts' // Order类型
+import { db } from '@/utils/indexDB.ts' // 数据库
+import { defaultTables } from '@/assets/constant.ts' // 默认桌台
+const currentOrderList = ref<Order[]>([]) // 当前订单列表
 // 获取当前订单列表
 const getCurrentOrderList = async () => {
-  currentOrderList.value = await db.order_store.where({ end_at: -1 }).toArray();
-};
-onMounted(() => getCurrentOrderList()); // 初始当前订单列表
+  currentOrderList.value = await db.order_store.where({ end_at: -1 }).toArray()
+}
+onMounted(() => getCurrentOrderList()) // 初始当前订单列表
 // 获取没有订单的桌台
 const currentNoOrderList = computed(() => {
-  const currentOrderTableIdList = currentOrderList.value.map((item) => item.table_id);
-  return defaultTables.filter((item) => !currentOrderTableIdList.includes(item.id));
-});
+  const currentOrderTableIdList = currentOrderList.value.map((item) => item.table_id)
+  return defaultTables.filter((item) => !currentOrderTableIdList.includes(item.id))
+})
 // 获取桌台名称
 const getTableName = (id: string | number) => {
-  return defaultTables.find((item) => item.id == id)?.name;
-};
+  return defaultTables.find((item) => item.id == id)?.name
+}
 // #endregion
 
 // #region 开台功能
 const clickTable = async (tableId: string | number) => {
-  const _timestamp = new Date().getTime(); // 开始时间
-  const _orderId = _timestamp + '' + tableId; // 订单id
-  const _tableItem = { name: '开台费', price: 0, number: 1, time_at: _timestamp }; // 默认开台费用
-  await db.order_store.add({ id: _orderId, table_id: tableId, start_at: _timestamp, end_at: -1, goods_list: [_tableItem] }); // 数据库新增数据
-  await getCurrentOrderList(); // 获取当前订单列表
-};
+  const _timestamp = new Date().getTime() // 开始时间
+  const _orderId = _timestamp + '' + tableId // 订单id
+  const _tableItem = { name: '开台费', price: 0, number: 1, time_at: _timestamp } // 默认开台费用
+  await db.order_store.add({ id: _orderId, table_id: tableId, start_at: _timestamp, end_at: -1, goods_list: [_tableItem] }) // 数据库新增数据
+  await getCurrentOrderList() // 获取当前订单列表
+}
 // #endregion
 
 // #region 计时功能
-import { useDateFormat, useTimestamp } from '@vueuse/core'; // 时间格式化，当前时间戳
-import { timestampDiffer } from '@/utils/format.ts'; // 时间戳相差
-const timestamp = useTimestamp({ offset: 0 });
+import { useDateFormat, useTimestamp } from '@vueuse/core' // 时间格式化，当前时间戳
+import { timestampDiffer } from '@/utils/format.ts' // 时间戳相差
+const timestamp = useTimestamp({ offset: 0 })
 // #endregion
 
 // #region 开台计算收费
-import { storeToRefs } from 'pinia';
-import { useChargeStore } from '@/store/charge.ts';
-const { chargeRatesArray } = storeToRefs(useChargeStore()); // 收费标准
+import { storeToRefs } from 'pinia'
+import { useChargeStore } from '@/store/charge.ts'
+const { chargeRatesArray } = storeToRefs(useChargeStore()) // 收费标准
 // 获取订单开台金额
 const getCurrentTablePrice = (order: Order) => {
-  const time: string = timestampDiffer(order.start_at, timestamp.value);
-  const _time: number = parseInt(time.slice(0, 2));
-  if (_time >= 24) return chargeRatesArray.value[23];
-  else return chargeRatesArray.value[_time];
-};
+  const time: string = timestampDiffer(order.start_at, timestamp.value)
+  const _time: number = parseInt(time.slice(0, 2))
+  if (_time >= 24) return chargeRatesArray.value[23]
+  else return chargeRatesArray.value[_time]
+}
 // 获取订单总金额
 const getCurrentOrderPrice = (order: Order) => {
-  let _num = 0;
-  order.goods_list.forEach((item) => (_num += item.price));
-  return _num;
-};
+  let _num = 0
+  order.goods_list.forEach((item) => (_num += item.price))
+  return _num
+}
 // 计算总价
 const sumPrice = (order: Order) => {
-  order.goods_list[0].price = getCurrentTablePrice(order);
-  return getCurrentOrderPrice(order);
-};
+  order.goods_list[0].price = getCurrentTablePrice(order)
+  return getCurrentOrderPrice(order)
+}
 // #endregion
 
 // #region 获取商品列表功能
-import type { Goods, Order_Goods } from '@/assets/type.ts'; // 商品类型，订单中商品类型
-import { goodsType } from '@/assets/constant.ts'; // 默认商品类型
-const currentGoodsTypeId = ref<number>(-1); // 当前商品类型id
-const currentGoodsList = ref<Goods[]>(); // 当前类型中的商品列表
+import type { Goods, Order_Goods } from '@/assets/type.ts' // 商品类型，订单中商品类型
+import { goodsType } from '@/assets/constant.ts' // 默认商品类型
+const currentGoodsTypeId = ref<number>(-1) // 当前商品类型id
+const currentGoodsList = ref<Goods[]>() // 当前类型中的商品列表
 // 监听商品类型切换，获取商品数据
 watch(currentGoodsTypeId, async (newValue) => {
-  if (newValue == -1) currentGoodsList.value = [];
-  else currentGoodsList.value = await db.goods_store.where({ type_id: newValue }).toArray();
-});
+  if (newValue == -1) currentGoodsList.value = []
+  else currentGoodsList.value = await db.goods_store.where({ type_id: newValue }).toArray()
+})
 // #endregion
 
 // #region 消费管理功能
-const defaultOrder = { id: '', table_id: '', start_at: -1, end_at: -1, goods_list: [], price: 0 }; // 默认订单
-const currentOrder = reactive<Order>({ id: '', table_id: '', start_at: -1, end_at: -1, goods_list: [], price: 0 }); // 当前订单
-const isShowTransfer = ref(false); // 是否打开新增消费穿梭框
+const defaultOrder = { id: '', table_id: '', start_at: -1, end_at: -1, goods_list: [], price: 0 } // 默认订单
+const currentOrder = reactive<Order>({ id: '', table_id: '', start_at: -1, end_at: -1, goods_list: [], price: 0 }) // 当前订单
+const isShowTransfer = ref(false) // 是否打开新增消费穿梭框
 // 显示消费穿梭框
 const onShowTransfer = (order: Order) => {
-  Object.assign(currentOrder, order);
-  currentOrder.goods_list[0].price = getCurrentTablePrice(order); // 开台费
-  isShowTransfer.value = true;
-};
+  Object.assign(currentOrder, order)
+  currentOrder.goods_list[0].price = getCurrentTablePrice(order) // 开台费
+  isShowTransfer.value = true
+}
 // 监听消费穿梭框显隐，
 watch(isShowTransfer, async (newValue) => {
-  if (newValue == true) return;
+  if (newValue == true) return
   else {
-    let _order = toRaw(currentOrder); // 取 proxy 值；存在双向绑定
-    if (!_order.id) return;
-    await db.order_store.update(_order.id, _order); // 更新数据库订单
-    getCurrentOrderList(); // 获取当前订单列表
-    Object.assign(currentOrder, defaultOrder); // 重置当前订单
+    let _order = toRaw(currentOrder) // 取 proxy 值；存在双向绑定
+    if (!_order.id) return
+    await db.order_store.update(_order.id, _order) // 更新数据库订单
+    getCurrentOrderList() // 获取当前订单列表
+    Object.assign(currentOrder, defaultOrder) // 重置当前订单
   }
-});
+})
 // 新增消费功能
 const addOrderGoods = (goods: Goods) => {
   const _goods: Order_Goods = {
     name: goods.name,
     price: goods.price,
     number: 1,
-    time_at: timestamp.value,
-  };
-  currentOrder.goods_list.push(_goods);
-};
+    time_at: timestamp.value
+  }
+  currentOrder.goods_list.push(_goods)
+}
 // 删除消费功能
 const deleteOrderGoods = async (goods: Order_Goods) => {
-  const index = currentOrder.goods_list.indexOf(goods); // 找到其位序
-  if (index !== -1) currentOrder.goods_list.splice(index, 1); // 从数组中删除元素
-};
+  const index = currentOrder.goods_list.indexOf(goods) // 找到其位序
+  if (index !== -1) currentOrder.goods_list.splice(index, 1) // 从数组中删除元素
+}
 // #endregion
 
 // #region 结算打印功能
-import Bill from '@/component/bill.vue';
-const isShowBill = ref(false); // 是否打开结账弹窗
-const printObj: any = ref({ id: 'printBill' }); // 打印设置：打印标签的ID
+import Bill from '@/component/bill.vue'
+const isShowBill = ref(false) // 是否打开结账弹窗
+// 打印设置：打印标签的ID
+const printObj: any = ref({
+  id: 'printBill',
+  // 开始打印之前的callback
+  beforeOpenCallback() {
+    closeOrder(currentOrder)
+  }
+})
 // 显示结账弹窗
 const onShowBill = (order: Order) => {
-  Object.assign(currentOrder, order);
-  currentOrder.end_at = timestamp.value; // 结束时间
-  currentOrder.goods_list[0].price = getCurrentTablePrice(order); // 开台费
-  currentOrder.price = getCurrentOrderPrice(order); // 总金额
-  isShowBill.value = true;
-};
-// 确定结账打印
-const printBill = (order: Order) => {
-  order = toRaw(order);
+  Object.assign(currentOrder, order)
+  currentOrder.end_at = timestamp.value // 结束时间
+  currentOrder.goods_list[0].price = getCurrentTablePrice(order) // 开台费
+  currentOrder.price = getCurrentOrderPrice(order) // 总金额
+  isShowBill.value = true
+}
+// 关闭订单
+const closeOrder = (order: Order) => {
+  order = toRaw(order)
   // 更新数据库
   db.order_store
-    .update(order.id, order)
+    .put(order, order.id)
     .then(() => {
-      getCurrentOrderList(); // 获取当前订单列表
-      isShowBill.value = false; // 关闭结账弹窗
+      getCurrentOrderList() // 获取当前订单列表
+      isShowBill.value = false // 关闭结账弹窗
+      ElMessage({
+        message: '订单已完成！',
+        type: 'success'
+      })
     })
-    .catch((error) => console.log(error));
-};
+    .catch((error) => {
+      console.log(error)
+      ElMessage.error(error)
+    })
+}
 // 监听结账弹窗显隐，
 watch(isShowBill, (newValue) => {
-  if (newValue == true) return;
-  else Object.assign(currentOrder, defaultOrder); // 重置当前订单
-});
+  if (newValue == true) return
+  else Object.assign(currentOrder, defaultOrder) // 重置当前订单
+})
 // #endregion
 </script>
--- src/pages/table/index.vue
+++ src/pages/table/index.vue
@@ -10,20 +10,14 @@
     <template v-if="item.type == 1"> {{ item.district[0] }} 到 {{ item.district[1] }} 小时内，每小时收费 {{ item.price }} 元 </template>
   </h2>
   <!-- 修改收费标准 -->
-  <el-dialog
-    v-model="isShowDialog"
-    title="修改收费标准"
-    width="980"
-    :show-close="false"
-    :close-on-click-modal="false"
-    :close-on-press-escape="false">
+  <el-dialog v-model="isShowDialog" title="修改收费标准" width="980" :show-close="false" :close-on-click-modal="false" :close-on-press-escape="false">
     <el-button type="primary" @click="addCharge"> + 新增时间阶段 </el-button>
     <div class="flex-row my-2" v-for="(item, index) in temporaryCharge" :key="index">
       <span>时间（小时）：</span>
       <el-input-number v-model="item.district[0]" :min="0" />
       <span class="mx-1">——</span>
       <el-input-number v-model="item.district[1]" :min="0" />
-      <el-select class="ml-4 mr-2" v-model="item.type" placeholder="收费方式">
+      <el-select class="ml-4 mr-2 max-w-40" v-model="item.type" placeholder="收费方式">
         <el-option label="区间收费" :value="0" />
         <el-option label="按时收费/小时" :value="1" />
       </el-select>
@@ -42,45 +36,42 @@
 /**
  * 显示收费标准
  */
-import { storeToRefs } from 'pinia';
-import { useChargeStore } from '@/store/charge.ts';
-const chargeStore = useChargeStore();
-const { chargeRates } = storeToRefs(chargeStore); // 当前订单
+import { storeToRefs } from 'pinia'
+import { useChargeStore } from '@/store/charge.ts'
+const chargeStore = useChargeStore()
+const { chargeRates } = storeToRefs(chargeStore) // 当前订单
 
 /**
  * 修改收费标准
  */
-import { Charge } from '@/assets/type.ts';
-const isShowDialog = ref(false); // 是否打开修改收费弹窗
-const temporaryCharge = ref<Charge[]>(); // 临时收费标准
-const { updateChargeRates } = chargeStore;
+import { Charge } from '@/assets/type.ts'
+const isShowDialog = ref(false) // 是否打开修改收费弹窗
+const temporaryCharge = ref<Charge[]>() // 临时收费标准
+const { updateChargeRates } = chargeStore
 watch(isShowDialog, (newValue) => {
-  if (newValue) temporaryCharge.value = JSON.parse(JSON.stringify(chargeRates.value));
-});
+  if (newValue) temporaryCharge.value = JSON.parse(JSON.stringify(chargeRates.value))
+})
 // 新增收费标准
 const addCharge = () => {
-  temporaryCharge.value?.push({ district: [0, 0], type: 0, price: 100 });
-};
+  temporaryCharge.value?.push({ district: [0, 0], type: 0, price: 100 })
+}
 // 删除某时段收费标准
 const deleteCharge = (index: number) => {
-  temporaryCharge.value?.splice(index, 1);
-};
+  temporaryCharge.value?.splice(index, 1)
+}
 // 确定更新收费标准
 const updateCharge = () => {
   if (!temporaryCharge.value || temporaryCharge.value.length < 1) {
-    ElMessage.error('收费标准不能为空');
-    return;
+    ElMessage.error('收费标准不能为空')
+    return
   }
   for (let i = 1; i < temporaryCharge.value.length; i++) {
-    if (
-      temporaryCharge.value[i].district[0] == temporaryCharge.value[i].district[1] ||
-      temporaryCharge.value[i].district[0] != temporaryCharge.value[i - 1].district[1]
-    ) {
-      ElMessage.error(`第${i + 1}阶段收费标准时间区间未正确设置`);
-      return;
+    if (temporaryCharge.value[i].district[0] == temporaryCharge.value[i].district[1] || temporaryCharge.value[i].district[0] != temporaryCharge.value[i - 1].district[1]) {
+      ElMessage.error(`第${i + 1}阶段收费标准时间区间未正确设置`)
+      return
     }
   }
-  updateChargeRates([...temporaryCharge.value]);
-  isShowDialog.value = false;
-};
+  updateChargeRates([...temporaryCharge.value])
+  isShowDialog.value = false
+}
 </script>
